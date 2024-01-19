4416059e76e73f2678e6c622d73e9a987ba37990
micc007 <mitro.michal123@gmail.com>
2024-01-19 13:08:03 +0100

    added nodemon/ts-node, basic Express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("ahoooooooooj")
+import express, { Express, Request, Response} from "express";
+
+let app: Express = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("route working");
+})
+
+app.listen(port, () => {
+    console.log("app listening");
+});
